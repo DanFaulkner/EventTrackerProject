a03b27eb9b80465868eed3f6f07a7d3738ed4580
Daniel Faulkner <danfaulkner0@gmail.com>
2022-01-07 15:40:41 -0700

    spring stuff

---
+++ LiftTracker/src/test/java/com/skilldistillery/lifttracker/LiftTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.lifttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
